f5c2ba9b9b52dd43ae413263d61fac69586a60e6
Jonathan Beckett <jonathan.beckett@gmail.com>
2022-10-27 10:49:41 +0100

    First commit!

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# obsidian_moc_generator
-Automatically generates Maps of Content for Obsidian vaults
+# Obsidian Map of Content Generator
+
+This is a python 3 script that automatically creates Map of Content markdown files for a directory tree full of markdown files.
+
+It generates a file called "folder name MoC.md" alongside each folder. Within each MoC file, is the title of the folder, and links to each markdown file within the folder.
+
+The script iterates through every folder below the passed in folder name, generating maps of content as it goes.
+
+Example:
+
+```python generate_mocs.py c:\my\vault
+
+You can of course run the script as many times as needed - it will just over-write the MoCs it has generated each time.
+
+*Note - if you have spaces on the end of any filenames, it will break the links in Obsidian - so be careful to clean your filenames up beforehand - there are a number of great free bulk filename tools around.*
---
+++ generate_mocs.py
@@ -0,0 +1,45 @@
+import os
+import sys
+from pathlib import Path
+
+def get_subdir_names(dir):
+    "Get a list of immediate subdirectories"
+    return next(os.walk(dir))[1]
+
+def get_subfile_names(dir):
+    "Get a list of immediate subfiles"
+    return next(os.walk(dir))[2]
+
+def spider(os_path,f_path):
+
+    subdir_names = get_subdir_names(os_path)
+    for subdir_name in subdir_names:
+        
+        os_subdir_path = os.path.join(os_path,subdir_name)
+        f_subdir_path = f_path + ("/" if f_path!="" else "")  + subdir_name
+
+        spider(os_subdir_path, f_subdir_path)
+        
+        moc_content = "# " + subdir_name + " Map of Contents\n\n"
+        subfile_names = get_subfile_names(os_subdir_path)
+        for file_name in filter(lambda sfn: Path(sfn).suffix == ".md", subfile_names):
+            moc_content += " - [[" + f_subdir_path + "/" + Path(file_name).stem + "|" + Path(file_name).stem + "]]\n"
+
+        moc_filename = os.path.join(os_path,subdir_name + " MoC.md")
+        print(moc_filename)
+        
+        with open(moc_filename, 'w') as f:
+            f.write(moc_content)
+
+
+if (len(sys.argv)==1):
+    print("\ngenerate_mocs.py - Obsidian Map of Contents Creation Tool, by Jonathan Beckett\n\nExample : python generate_mocs.py c:\\my\\vault\\root\\folder\n\n")
+    sys.exit(0)
+
+init_path = sys.argv[1]
+
+if not os.path.isdir(init_path):
+    print("The directory (" + init_path +") does not exist")
+    sys.exit(0)
+
+spider(init_path,"")
